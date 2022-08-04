228132fefd9684b8f2d7012f638968a0e07f5dba
jaechanyoon <wocksl1998@gmail.com>
2022-08-04 18:05:05 +0900

    solve

---
+++ 크레인인형뽑기게임.py
@@ -0,0 +1,15 @@
+def solution(board, moves):
+    basket = []
+    answer = 0
+    for i in moves:
+        for j in range(len(board)):
+            if board[j][i-1] != 0:
+                basket.append(board[j][i-1])
+                board[j][i - 1] = 0
+                if len(basket) > 1:
+                    if basket[-2] == basket[-1]:
+                        basket.pop(-1)
+                        basket.pop(-1)
+                        answer += 2
+                break
+    return answer
